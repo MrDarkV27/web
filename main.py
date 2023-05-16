e75a839206dc05fef8d87fa0cc4dcee2985a0e48
MrDarkV27 <133194809+MrDarkV27@users.noreply.github.com>
2023-05-16 17:37:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import requests
 import time
 
 def make_request():
-    url = 'https://amodz.online'  # Replace with the desired URL
-
-    # Send GET request
+    url = 'https://amodz.online'  
     response = requests.get(url)
    
 
 while True:
     make_request()
-    time.sleep(001)  # Delay for 200 seconds
+   
